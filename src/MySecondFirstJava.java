ce1a26e7bd0595867b3b5be83ea138074a018ee8
rap9210 <rap9210@gmail.com>
2020-04-20 15:02:56 -0400

    MySecondFirstJava

---
+++ src/MySecondFirstJava.java
@@ -0,0 +1,6 @@
+public class MySecondFirstJava {
+    public static void main(String[] args){
+        System.out.println("Hello World.");
+        System.out.println("Today is April 20, 2020, and I am alive.");
+    }
+}
